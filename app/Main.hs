16fcd1e82444740705bdf07e68079c325c036ea1
igorstec <igor@tmsaudit.pl>
2026-04-15 16:52:59 +0200

    create initial Parser for HsModule

--- app/Main.hs
+++ app/Main.hs
@@ -1,39 +1,104 @@
 module Main where
-    import qualified Data.Map as Map
 
-    infixl 9 :$
+import qualified Data.Map as Map
+import System.Environment (getArgs)
 
-    --Opis danych w jaki sposób chcemy miec dane naszego programu w formie definicji funkcji DEF zawarte w liscie o nazwie
-    -- Prog, sama funkcja ma miec nazwe stringa liste argumentów [Pat] i ciao funckji Expr które jest drzewem binarnym
-    data Def = Def Name [Pat] Expr
-    data Expr = Var Name | Expr :$ Expr
-    type Pat = Name
-    type Name = String
+import Language.Haskell.Syntax
+import Language.Haskell.Parser
+--import Data.Type.Coercion (trans)
 
-    newtype Prog = Prog {progDefs :: [Def]}
+infixl 9 :$
 
-    --ta funckja zamienia liste Prog którą mósimy stworzyc z danych na mape definicji funkcji
-    type DefMap = Map.Map Name Def
+-- Opis danych w jaki sposób chcemy miec dane naszego programu w formie definicji funkcji DEF zawarte w liscie o nazwie
+-- Prog, sama funkcja ma miec nazwe stringa liste argumentów [Pat] i ciao funckji Expr które jest drzewem binarnym
+data Def = Def Name [Pat] Expr
+data Expr = Var Name | Expr :$ Expr
+type Pat = Name
+type Name = String
 
-    buildDefMap :: Prog -> DefMap
-    buildDefMap (Prog defs) = Map.fromList [ (name, d) | d@(Def name _ _) <- defs ]
+newtype Prog = Prog {progDefs :: [Def]}
 
-    --funckja która zamienia skladnie podzbioru haskela na string w formie ktora nas interesuje
-    fromHsString :: String -> [Def]
-    fromHsString x = []
+-- Ta funckja zamienia liste Prog którą musimy stworzyc z danych na mape definicji funkcji
+type DefMap = Map.Map Name Def
 
-    -- z parseREsult Hsmodule na Prog
-    fromParseResult :: ParseResult HsModule -> [Def]
+buildDefMap :: Prog -> DefMap
+buildDefMap (Prog defs) = Map.fromList [ (name, d) | d@(Def name _ _) <- defs ]
 
-    --z modułu na tal na prawde Prog
-    fromHsModule :: HsModule -> [Def]
+-- Funkcja która zamienia skladnie podzbioru haskela na string w formie ktora nas interesuje
+-- To bedzie dlugir ja mam stringa kombinatorw chce je dac na hsmodule to ParseResult HSModule itd. az [Def]
+fromHsString :: String -> Prog
+fromHsString = Prog . fromParseResult . parseModule
+
+-- (Zakomentowane do czasu importu z haskell-src)
+fromParseResult :: ParseResult HsModule -> [Def]
+fromParseResult x = case x of
+    ParseOk hsModule -> fromHsModule hsModule
+    ParseFailed errloc err -> error $ "Blad parsowania: " ++ err ++ " w miejscu" ++ show errloc ++ "\n"
+
+
+--Tu dostaje juz duzy skomplikowany HsModule i musze go przerobic na liste Def, czyli na nasz wewnetrzny format
+-- Nasz Prog to opakowanie na liste Def, a Def to nazwa funkcji, lista argumentów i ciało funkcji, czyli Expr
+-- zakładamy ze naazwy funkcji sa unikalne, a argumenty sa niepowtarzajace sie. w jednej definijcii funkcji nie moze byc powtarzajacych sie argumentow, ale w roznych definicjach juz tak
+--HsModule SrcLoc Module (Maybe [HsExportSpec]) [HsImportDecl] [HsDecl]	 
+
+fromHsModule :: HsModule -> [Def]
+fromHsModule (HsModule _ _ _ _ decls) = concatMap transDecl decls
+
+-- Nasza funkcja pomocnicza:
+transDecl :: HsDecl -> [Def]
+transDecl (HsFunBind matches) = map transMatch matches
+transDecl _                   = []  -- Ignorujemy wszystko inne! Zwracamy pustą listę.
+
+--HsMatch SrcLoc HsName [HsPat] HsRhs [HsDecl]	 
+--data Def = Def Name [Pat] Expr
+transMatch :: HsMatch -> Def
+transMatch (HsMatch _ name pats rhs _) = Def (transName name) (map transPat pats) (transRhs rhs)
+
+transName :: HsName -> Name
+transName = show
+
+transPat ::HsPat -> Pat
+transPat = show
+
+transRhs :: HsRhs -> Expr
+transRhs (HsUnGuardedRhs expr) = transExp expr
+transRhs _ = error "Nie przyjmujemy definicji z guardsami."
+
+transExp :: HsExp -> Expr
+transExp (HsApp lewe prawe) = transExp lewe :$ transExp prawe
+transExp (HsVar (UnQual (HsIdent nazwa))) = Var nazwa
+transExp (HsParen srodek) = transExp srodek
+transExp _ = error "Skladnia nieobslugiwana w tym jezyku!"
 
 
 
-    -- Sciezka refukcji
-    rpath :: Expr -> [Expr]
-    rpath e = e : maybe [] rpath (rstep e)
+-- Ścieżka redukcji
+rpath :: Expr -> [Expr]
+rpath e = e : maybe [] rpath (rstep e)
 
-    rstep :: Expr -> Maybe Expr
-    rstep (I :$ x)                = Just x
+-- Pojedyńcze przejście
+rstep :: Expr -> Maybe Expr
+rstep (Var "i" :$ x) = Just x
+rstep _              = Nothing
 
+-- GŁÓWNA FUNKCJA PROGRAMU
+main :: IO ()
+main = do
+    args <- getArgs
+    case args of
+        [plik] -> do
+            -- Wczytujemy zawartość pliku do zmiennej typu String
+            kodZrodlowy <- readFile plik
+            
+            -- Tutaj w przyszłości połączysz to z parserem, np.:
+            let definicje = fromHsString kodZrodlowy
+            let mapa = buildDefMap definicje
+            putStrLn "Zbudowano mapę!"
+            
+            -- Na razie tylko potwierdzamy, że plik się wczytał:
+            putStrLn $ "Udalo sie wczytac plik: " ++ plik
+            putStrLn "Oto jego zawartosc:"
+            putStrLn kodZrodlowy
+            
+        _ -> 
+            putStrLn "Blad argumentow."
